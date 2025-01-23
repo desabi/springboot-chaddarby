4cb0fb53da74f528ce08f0fcfe5560ebfc33ed59
desabi <desabi@live.com>
2025-01-23 13:30:49 -0600

    feat: my-basic-project, initial commit

---
+++ my-basic-project/src/main/java/com/desabisc/udemy/chad/MyBasicProjectApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.udemy.chad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBasicProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyBasicProjectApplication.class, args);
+	}
+
+}
---
+++ my-basic-project/src/test/java/com/desabisc/udemy/chad/MyBasicProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.udemy.chad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBasicProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
