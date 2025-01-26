d6f984234be30ea61395c021a9ef11f32c7f28f8
desabi <desabi@live.com>
2025-01-26 07:13:32 -0600

    spring core eg, initial commit

---
+++ spring-core-eg/src/main/java/com/desabisc/udemy/chad/SpringCoreEgApplication.java
@@ -0,0 +1,13 @@
+package com.desabisc.udemy.chad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCoreEgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreEgApplication.class, args);
+	}
+
+}
