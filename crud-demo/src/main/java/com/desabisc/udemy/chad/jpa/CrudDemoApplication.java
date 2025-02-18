b3d56577eb259e703688370afef90f2b0ed09303
desabi <desabi@live.com>
2025-02-18 14:21:13 -0600

    feat: crud-demo, initial config

--- crud-demo/src/main/java/com/desabisc/udemy/chad/jpa/CrudDemoApplication.java
+++ crud-demo/src/main/java/com/desabisc/udemy/chad/jpa/CrudDemoApplication.java
@@ -1,7 +1,9 @@
 package com.desabisc.udemy.chad.jpa;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CrudDemoApplication {
@@ -10,4 +12,16 @@ public class CrudDemoApplication {
 		SpringApplication.run(CrudDemoApplication.class, args);
 	}
 
+	/**
+	 * Executed after the Spring Beans have been loaded.
+	 *
+	 * @param args
+	 * @return
+	 */
+	@Bean
+	public CommandLineRunner commandLineRunner(String[] args) {
+		return runner -> {
+			System.out.println("Hello world");
+		};
+	}
 }
