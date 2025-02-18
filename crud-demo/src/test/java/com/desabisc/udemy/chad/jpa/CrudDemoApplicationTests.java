8c65deef8853701040ebdc5b181acfca00937d19
desabi <desabi@live.com>
2025-02-18 09:44:09 -0600

    feat: crud-demo, add base project

---
+++ crud-demo/src/test/java/com/desabisc/udemy/chad/jpa/CrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.udemy.chad.jpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
