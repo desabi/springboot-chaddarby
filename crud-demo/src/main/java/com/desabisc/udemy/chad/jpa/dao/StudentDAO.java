bd3802d43cba957b2919c57480b831ee4e2404e8
desabi <desabi@live.com>
2025-02-27 19:30:08 -0600

    feat: crud-demo, update and delete eg

--- crud-demo/src/main/java/com/desabisc/udemy/chad/jpa/dao/StudentDAO.java
+++ crud-demo/src/main/java/com/desabisc/udemy/chad/jpa/dao/StudentDAO.java
@@ -8,4 +8,7 @@ public interface StudentDAO {
   Student findById(Integer id);
   List<Student> findAll();
   List<Student> findByLastName(String theLastName);
+  void update(Student theStudent);
+  void delete(Integer id);
+  int deleteAll();
 }
