5537ee1f2485789738407881b4a043a476b43d21
desabi <desabi@live.com>
2025-03-08 11:16:02 -0600

    feat: employees-crud, base project

---
+++ employees-crud/src/test/java/com/desabisc/udemy/chad/employees/crud/EmployeesCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desabisc.udemy.chad.employees.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
