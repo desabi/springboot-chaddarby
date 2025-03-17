c5f2c87ccece2415b78ec0f45d8696f9eb8ad323
desabi <desabi@live.com>
2025-03-17 14:01:40 -0600

    feat: employees-crud, spring data rest options

--- employees-crud/src/main/java/com/desabisc/udemy/chad/employees/crud/dao/EmployeeRepository.java
+++ employees-crud/src/main/java/com/desabisc/udemy/chad/employees/crud/dao/EmployeeRepository.java
@@ -2,8 +2,8 @@ package com.desabisc.udemy.chad.employees.crud.dao;
 
 import com.desabisc.udemy.chad.employees.crud.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@Repository
+//@RepositoryRestResource(path = "members") // localhost:8080/magic-api/members
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
